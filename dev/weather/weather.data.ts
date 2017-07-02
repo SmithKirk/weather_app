582dcb1c063f6e424c1f904301aecd0829db6eb9
Kirk Smith <kirks@hotmail.co.uk>
2017-07-02 12:18:21 +0100

    weather list component now loops through weather data component

---
+++ dev/weather/weather.data.ts
@@ -0,0 +1,5 @@
+import {WeatherItem} from './weather-item';
+export const WEATHER_ITEMS: WeatherItem[] = [
+    new WeatherItem('London', 'Rainy', 9),
+    new WeatherItem('Paris', 'Windy', 12),
+];
