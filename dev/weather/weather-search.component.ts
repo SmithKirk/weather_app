118b5e88b3334433c1b3a9032cbfbf8a55f895c6
Kirk Smith <kirks@hotmail.co.uk>
2017-07-03 20:11:41 +0100

    Weather service can make request to Openweather

--- dev/weather/weather-search.component.ts
+++ dev/weather/weather-search.component.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core';
-import {ControlGroup} from './angular2/common';
+import {ControlGroup} from 'angular2/common';
+import {WeatherService} from './weather.service';
+import {WeatherItem} from './weather-item';
+
 @Component({
     selector: 'weather-search',
     template: `
@@ -17,7 +20,15 @@ import {ControlGroup} from './angular2/common';
 })
 export class WeatherSearchComponent
 {
+    constructor (private _weatherService: WeatherService) {}
+
     onSubmit(form: ControlGroup) {
-        console.log(form);
+        this._weatherService.searchWeatherData(form.value.location)
+            .subscribe(
+                data => {
+                    const weatherItem = new WeatherItem(data.name, data.weather[0].description, data.main.temp);
+                    this._weatherService.addWeatherItem(weatherItem);
+                }
+            );
     }
 }
--- dev/weather/weather.data.ts
+++ dev/weather/weather.data.ts
@@ -1,5 +1,4 @@
 import {WeatherItem} from './weather-item';
 export const WEATHER_ITEMS: WeatherItem[] = [
-    new WeatherItem('London', 'Rainy', 9),
-    new WeatherItem('Paris', 'Windy', 12),
+    
 ];
